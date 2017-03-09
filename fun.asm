f3e6b9d0cb358f8e6cfe84bcb64fd44af490e465
fzanollo <fzanollo@dc.uba.ar>
2017-03-09 15:49:49 -0300

    cuenta correctamente los picos

--- fun.asm
+++ fun.asm
@@ -1,7 +1,9 @@
 global fun
 
+section .data
 %define tam_elem 4
 %define pack_size 4
+one: dd 1.0
 
 section .text
 
@@ -20,14 +22,9 @@ push r15
 ;rsi = rows
 ;rdx = cols
 
-mov r12, 1
-vmovq xmm10, r12
-movaps xmm1,xmm10
-PUNPCKLDQ xmm10,xmm1
-movaps xmm1,xmm10
-PUNPCKLDQ xmm10,xmm1
-movaps xmm1,xmm10
-PUNPCKLDQ xmm10,xmm1
+XORPD xmm10, xmm10
+movss xmm10, [one]
+shufps xmm10, xmm10, 0x0 ; xmm10 tiene todos 1.0s (float)
 
 XORPD xmm11, xmm11 ; xmm11 tiene todos 0s (lo uso para comparar)
 
@@ -53,8 +50,8 @@ mov r9, 0
 		movdqa xmm3, xmm2 ; xmm3 para cant montanias
 
 		;en xmm3 dejo 1 si hay montania 0 si no
-		cmpps xmm3, xmm11, 4 ; xmm3= en cada float: 1111 1111 si > 0, 0s cc
-		psrld xmm3, 31 ; xmm3= en cada float: 1 si > 0, 0 cc
+		cmpps xmm3, xmm11, 4 ; xmm3= en cada float: 1..1 si > 0, 0s cc
+		pand xmm3, xmm10 ; xmm3= en cada float: 1 si > 0, 0 cc
 
 		;siguiente fila de elems
 		inc r8
@@ -68,11 +65,8 @@ mov r9, 0
 		movdqa xmm5, xmm4 ; xmm5 para cant montanias
 
 		;en xmm5 dejo 1 si hay montania 0 si no
-		cmpps xmm5, xmm11, 4 ; xmm5= en cada float: 1111 1111 si > 0, 0s cc
-		
-
-		;DEBUG
-		movdqa xmm1, xmm5
+		cmpps xmm5, xmm11, 4 ; xmm5= en cada float: 1..1 si > 0, 0s cc
+		pand xmm5, xmm10 ; xmm5= en cada float: 1 si > 0, 0 cc
 
 		;Datos obtenidos
 		;suma de influencias
@@ -85,7 +79,7 @@ mov r9, 0
 		;xmm0 para influencia
 		addps xmm0, xmm2 
 		;xmm1 para cant montanias
-		; addps xmm1, xmm3 
+		addps xmm1, xmm3 
 
 		;ir al sig:
 		inc r8
@@ -94,7 +88,7 @@ mov r9, 0
 
 	;calculo final del terreno (influencia/cant picos)
 
-	movdqa [rdi + r9 * tam_elem], xmm10
+	movdqa [rdi + r9 * tam_elem], xmm1
 
 	;ir al sig:
 	add r9, pack_size
